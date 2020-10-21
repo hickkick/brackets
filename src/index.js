1e24eba8e25af7b206a7e26774f602acb87dc263
Romanoli <inthewavves@gmail.com>
2020-10-22 00:14:38 +0300

    add: implement  task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  arr = str.split("");
+
+  for(let i = 0; i < arr.length; i++) {
+      for(let k = 0; k < bracketsConfig.length; k++) {
+            
+        if (arr[i] == bracketsConfig[k][0] && arr[i + 1] == bracketsConfig[k][1]) {
+            arr.splice(i, 2);
+            i = -1;
+        };
+
+        if (arr.length == 0) {
+            return true
+        };
+      };  
+  };
+
+  return false;
 }
